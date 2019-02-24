e228b045d848e414e74433cad7d008d9c7ad1d0f
Wynn Marlar Khaing <wynnmarlarkhaing.1993@gmail.com>
2019-02-24 18:32:09 +0630

    admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor' )
+    list_display_links = ('title',)
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title','photo_1','address', 'city', 'state','zipcode','price')
+    list_per_page = 2
+
+admin.site.register(Listing,ListingAdmin)
+
